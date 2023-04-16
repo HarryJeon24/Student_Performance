4cbab94366eb780cdcf3c9846969554671f9ce87
hjeon62@emory.edu <hjeon62@emory.edu>
2023-04-16 10:58:49 -0400

    preprocess_final

--- preprocess_final.py
+++ preprocess_final.py
@@ -4,7 +4,10 @@ import pandas as pd
 # Load the data
 train_data = pd.read_csv('preprocessed.csv')
 
-data = train_data[['session_id','level_group','room_coor_y_mean', 'room_coor_x_mean', 'screen_coor_x_mean', 'elapsed_time_sum', 'screen_coor_y_mean', 'level_mean', 'music_max', 'name_nunique','room_fqid_nunique', 'event_name_nunique','fqid_count', 'question_number', 'correct']]
+data = train_data[
+    ['session_id', 'question_number', 'level_group', 'room_coor_y_mean', 'room_coor_x_mean', 'screen_coor_x_mean',
+     'elapsed_time_sum', 'screen_coor_y_mean', 'level_mean', 'music_max', 'name_nunique', 'room_fqid_nunique',
+     'event_name_nunique', 'fqid_count', 'correct']]
 
 # Save the data as CSV
 filename = "preprocessed_final.csv"
@@ -14,28 +17,28 @@ data.to_csv(filename, index=False)
 data = pd.read_csv('preprocessed_final.csv')
 
 # Specify the columns you want to use for training and testing
-features = ['room_coor_y_mean', 'room_coor_x_mean', 'screen_coor_x_mean', 'elapsed_time_sum', 'screen_coor_y_mean', 'level_mean', 'music_max', 'name_nunique','room_fqid_nunique', 'event_name_nunique','fqid_count']
+features = ['question_number', 'room_coor_y_mean', 'room_coor_x_mean', 'screen_coor_x_mean', 'elapsed_time_sum', 'screen_coor_y_mean',
+            'level_mean', 'music_max', 'name_nunique', 'room_fqid_nunique', 'event_name_nunique', 'fqid_count']
 target = 'correct'
 
-# Group the data by session_id and question_number
-grouped_data = data.groupby(['session_id', 'question_number'])
+# Group the data by session_id
+grouped_data = data.groupby('session_id')
 
 # Initialize empty dataframes for train and test data
 train_data = pd.DataFrame(columns=data.columns)
 test_data = pd.DataFrame(columns=data.columns)
 
-# For each group, predict the correct column based on previous information within the group
+# Split data within each session_id group while maintaining the relationship between question_number and correct
 for _, group in grouped_data:
-    if len(group) > 1:
-        # Split the data within the group into train and test
-        group_train_data, group_test_data = train_test_split(group, test_size=0.2, random_state=42)
+    group = group.sort_values('question_number')
 
-        # Append the split data to the respective train and test dataframes using pandas.concat
-        train_data = pd.concat([train_data, group_train_data])
+    if len(group) > 1:
+        group_train_data, group_test_data = train_test_split(group, test_size=0.2, random_state=42, shuffle=False)
         test_data = pd.concat([test_data, group_test_data])
     else:
-        # If there is only one sample in the group, add it to the train set using pandas.concat
-        train_data = pd.concat([train_data, group])
+        group_train_data = group
+
+    train_data = pd.concat([train_data, group_train_data])
 
 # Split the data into features and labels
 train_features = train_data[features]
